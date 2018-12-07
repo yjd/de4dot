f3727a2d18359703550a934e10b8139701daacb0
Tianjiao <wangtianjiao.wang959@gmail.com>
2018-12-07 20:16:25 +0800

    Create InstallNETCoreSDK.ps1

---
+++ InstallNETCoreSDK.ps1
@@ -0,0 +1,3 @@
+# https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script
+
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1')))"
